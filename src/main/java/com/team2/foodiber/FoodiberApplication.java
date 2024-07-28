4b986608ef1b0718354e09543236cd41d0a73685
Ken <michelsonken@gmail.com>
2024-07-28 11:55:22 +0300

    update

---
+++ src/main/java/com/team2/foodiber/FoodiberApplication.java
@@ -0,0 +1,13 @@
+package com.team2.foodiber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team2/foodiber/FoodiberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.foodiber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
