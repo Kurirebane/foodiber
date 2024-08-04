dab61305f437461d15e2ccfbcd463c4580e82586
mariamuuli <mariamuuli91@gmail.com>
2024-08-04 12:08:28 +0300

    category searching html mapping in maincontroller

--- src/main/java/com/team2/foodiber/controller/MainController.java
+++ src/main/java/com/team2/foodiber/controller/MainController.java
@@ -29,6 +29,11 @@ public class MainController {
         return "index";
     }
 
+    @GetMapping(path = "/select-category")
+    public String getAllCategories(){
+        return "select-category";
+    }
+
 
 
 
