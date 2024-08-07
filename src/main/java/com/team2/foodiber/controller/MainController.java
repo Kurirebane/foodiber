3d4a366c28485d797362e07a8d05965a9496e0e9
mariamuuli <mariamuuli91@gmail.com>
2024-08-07 20:39:30 +0300

    select category buttons -> category page

--- src/main/java/com/team2/foodiber/controller/MainController.java
+++ src/main/java/com/team2/foodiber/controller/MainController.java
@@ -1,11 +1,14 @@
 package com.team2.foodiber.controller;
 
 import com.team2.foodiber.model.Recipe;
+import com.team2.foodiber.model.RecipeCategory;
 import com.team2.foodiber.service.RecipeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -34,6 +37,18 @@ public class MainController {
 
         return "select-category";
     }
+
+
+    @GetMapping("/category")
+    public String getCategoryPage(@RequestParam(name = "name") RecipeCategory recipeCategory, ModelMap model) {
+        List<Recipe> filteredRecipes = recipeService.getRecipesByCategory(recipeCategory);
+        model.addAttribute("recipes", filteredRecipes);
+        model.addAttribute("categoryName", recipeCategory);
+        return "category";
+    }
+
+
+
 
 
 //    @GetMapping(path = )
--- src/main/java/com/team2/foodiber/repository/RecipeRepository.java
+++ src/main/java/com/team2/foodiber/repository/RecipeRepository.java
@@ -1,11 +1,15 @@
 package com.team2.foodiber.repository;
 
 import com.team2.foodiber.model.Recipe;
+import com.team2.foodiber.model.RecipeCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RecipeRepository extends JpaRepository <Recipe, Long> {
 
 
+List <Recipe> findByRecipeCategory(RecipeCategory recipeCategory);
 }
