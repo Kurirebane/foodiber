5572df4c31eb770909ca4448dd0cf54b35e838aa
Ken <michelsonken@gmail.com>
2024-08-15 19:41:55 +0300

    create-recipe.html + user API for creating recipes.

--- src/main/java/com/team2/foodiber/repository/ImageRepository.java
+++ src/main/java/com/team2/foodiber/repository/ImageRepository.java
@@ -3,8 +3,9 @@ package com.team2.foodiber.repository;
 
 import com.team2.foodiber.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
 }
