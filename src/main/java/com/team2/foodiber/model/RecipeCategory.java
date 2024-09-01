87e9e4d8460cd53d043fe31de60a944c2f474bc6
mariamuuli <mariamuuli91@gmail.com>
2024-09-01 20:41:26 +0300

    restyling and adding create recipe button

--- src/main/java/com/team2/foodiber/model/RecipeCategory.java
+++ src/main/java/com/team2/foodiber/model/RecipeCategory.java
@@ -1,5 +1,31 @@
 package com.team2.foodiber.model;
 
+import lombok.Getter;
+
+@Getter
 public enum RecipeCategory {
-    BREAKFAST, MAIN_COURSE, SOUP, DESSERT, VEGETARIAN
+    BREAKFAST("BREAKFast"),
+    MAIN_COURSE("MAIN COURSE"),
+    SOUP("SOUP"),
+    DESSERT("DESSERT"),
+    VEGETARIAN("VEGETARIAN");
+
+    private final String dpName;
+
+    RecipeCategory(String dpName){
+        this.dpName = dpName;
+    }
+
+    public static RecipeCategory fromString(String text){
+        return switch (text) {
+            case "BREAKFAST" -> BREAKFAST;
+            case "MAIN COURSE" -> MAIN_COURSE;
+            case "SOUP" -> SOUP;
+            case "DESSERT" -> DESSERT;
+            case "VEGETARIAN" -> VEGETARIAN;
+            default -> throw new IllegalArgumentException("Unknown recipe category" + text);
+        };
+
+    }
 }
+
